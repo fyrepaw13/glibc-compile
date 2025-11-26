691c61178420365483ed0a60371c0736fc43cd93
fyrepaw13 <62428064+fyrepaw13@users.noreply.github.com>
2025-11-26 16:53:16 +0800

    Update build-and-compile.sh

--- build-and-compile.sh
+++ build-and-compile.sh
@@ -3,7 +3,7 @@ set -e
 
 IMAGE_NAME="ubuntu1804_glibc227"     # can change this to anything you want
 CONTAINER_NAME="glibc_build_temp"    # can change this to anything you want
-HOST_BINARY_NAME="test"              # can change this to anything you want
+HOST_BINARY_NAME="playground"              # can change this to anything you want
 
 # Build the image from the current directory
 echo "[*] Building Docker image..."
@@ -17,10 +17,16 @@ sudo docker run --name "$CONTAINER_NAME" "$IMAGE_NAME"
 echo "[*] Copying binary from container..."
 sudo docker cp "$CONTAINER_NAME":/work/$HOST_BINARY_NAME "./$HOST_BINARY_NAME"
 
+# Copy the libc from the container to host
+echo "[*] Copying libc from container..."
+sudo docker cp "$CONTAINER_NAME":/lib/x86_64-linux-gnu/libc.so.6 "./libc.so.6"
+
 # Remove the container
 echo "[*] Cleaning up..."
 sudo docker rm "$CONTAINER_NAME" > /dev/null
 
 echo "[+] Done! Binary saved as $HOST_BINARY_NAME"
+
+sudo chown kali:kali libc.so.6
 
 pwninit --no-template
